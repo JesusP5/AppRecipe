905bdb2c648cc132c56aeff1ebfd1380db4dab6c
rodrex25 <gabresh25gmail.com>
2024-03-01 18:47:10 -0600

    feat: added small buton component

--- RecipeApp/App.jsx
+++ RecipeApp/App.jsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import  SmallButton from './components/BasicButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>Components small button show</Text>
+      <SmallButton title="Press me"/>
+
       <StatusBar style="auto" />
     </View>
   );
---
+++ RecipeApp/components/BasicButton.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+
+
+
+function SmallButton  ({ onPress, title })  {
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.TouchableOpacity}>
+          <Text style={styles.Text}>{title}</Text>
+        </TouchableOpacity>
+      );
+    };
+
+const styles = StyleSheet.create({ 
+
+    TouchableOpacity: {
+        backgroundColor: '#F1F1F1',
+        padding: 10,
+        borderRadius: 10,
+          },
+
+    Text: {
+        color: '#25242A',
+        fontSize: 15,
+        fontFamily: '',
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 1,
+        marginBottom: 1,
+        textAlign: 'center',
+    },
+    });
+
+export default SmallButton;
