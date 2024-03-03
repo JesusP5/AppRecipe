22fdaf205f5959fca77ff7e4e96bdb822cf29405
rodrex25 <gabresh25gmail.com>
2024-03-02 19:53:26 -0600

    feat: added OnSearchElements

--- RecipeApp/App.jsx
+++ RecipeApp/App.jsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import  {SmallButton, RoundButton} from './components/BasicButton';
 import FoodCard from './components/FoodCard';
 import { SearchBar,OnSearchBar } from './components/SearchBar';
+import {Recomendation, Recent}  from './components/SearchElement';
 
 export default function App() {
   return (
@@ -14,9 +15,14 @@ export default function App() {
       <Text>Components food card show</Text>
       <FoodCard title="Pasta´s House" description="Pasta | Italiana" image={require('./assets/FoodImgExample.jpg')}/>
       <StatusBar style="auto" />
-      <Text>Components SearchBar and OnSearchBar</Text>
+      <Text>Components SearchBar and OnSearchBar show</Text>
       <SearchBar placeholder="Buscar una receta"/>
       <OnSearchBar placeholder="Buscar" />
+      <Text>Components OnSearchElements and OnSearchBar show</Text>
+      <Recomendation title = "Burger king"/>
+      <Recent title='Arroz'/>
+      
+
     </View>
   );
 }
