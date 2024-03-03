2b02d50b1122c99bd4de133459da08b70b3eb638
RodriiTepatzi <rodrif19@gmail.com>
2024-03-03 12:10:30 -0600

    fix: fixing status bar and adding fixed width to bottombar

--- RecipeApp/App.jsx
+++ RecipeApp/App.jsx
@@ -1,11 +1,12 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import DashboardScreen from './screens/dashboardScreen';
+import { SafeAreaView, StyleSheet, } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import TabScreen from './screens/tabScreen';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style="auto" />
       <SafeAreaView style={styles.container}>
         <TabScreen/>
       </SafeAreaView>
