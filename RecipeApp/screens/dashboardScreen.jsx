af7aa44694d693683a6baa04681fc6544b3fb5a0
alanolv <olvera.cristino.alan@gmail.com>
2024-03-03 18:03:57 -0600

    feat:Add summaryScreen

--- RecipeApp/screens/dashboardScreen.jsx
+++ RecipeApp/screens/dashboardScreen.jsx
@@ -91,6 +91,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "start",
     justifyContent: "start",
+    marginTop: 25,
   },
   title: {
     fontFamily: "MoreSugar",
--- RecipeApp/screens/summaryScreen.jsx
+++ RecipeApp/screens/summaryScreen.jsx
@@ -1,20 +1,27 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, Keyboard} from "react-native";
 import {
-    Gesture,
-    GestureDetector,
-    ScrollView,
-  } from "react-native-gesture-handler";
+  Gesture,
+  GestureDetector,
+  ScrollView,
+} from "react-native-gesture-handler";
 
-export default function SummaryScreen({selectedElement}) {
-    const dismissOnTap = Gesture.Tap().onEnd(() => Keyboard.dismiss());
-    const composed = Gesture.Simultaneous(dismissOnTap);
-    const data = selectedElement;
+export default function SummaryScreen({ selectedElement }) {
+  const dismissOnTap = Gesture.Tap().onEnd(() => Keyboard.dismiss());
+  const composed = Gesture.Simultaneous(dismissOnTap);
+  const data = selectedElement;
+  const instructionsWithoutTags = data.instructions.replace(/<[^>]*>/g, '');
   return (
     <GestureDetector gesture={composed}>
       <View style={styles.container}>
-        <Text>{data.title}</Text>
+        <View style={styles.topScreen}>
+          <Text style={styles.title}>{data.title}</Text>
+          <Image source={{ uri: data.image }} style={styles.image} />
+        </View>
+        <View style={styles.botScreen}>
+          <Text style={styles.titleSumary}>Recipe:</Text>
+          <Text>{instructionsWithoutTags}</Text>
+        </View>
       </View>
     </GestureDetector>
   );
@@ -23,15 +30,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "start",
-    justifyContent: "start",
+    padding: 20,
+    marginTop: 20,
   },
   title: {
     fontFamily: "MoreSugar",
-    fontSize: 28,
+    fontSize: 24,
+    marginVertical: 20,
+    marginHorizontal: 30,
+  },
+  titleSumary: {
+    fontFamily: "MoreSugar",
+    fontSize: 20,
     marginVertical: 20,
   },
-  contentMargin: {
-    marginHorizontal: 50,
+  topScreen: {
+    flex: 1,
+    alignItems: "center",
+  },
+  botScreen: {
+    flex: 1,
+  },
+  image: {
+    width: "90%",
+    height: "75%",
+    borderRadius: 10,
+    margin:0,
   },
 });
