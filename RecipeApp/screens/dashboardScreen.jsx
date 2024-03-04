0382512d261bf98410053fc3b2dee380caac2f7b
RodriiTepatzi <rodrif19@gmail.com>
2024-03-03 21:06:04 -0600

    feat: adding simples

--- RecipeApp/screens/dashboardScreen.jsx
+++ RecipeApp/screens/dashboardScreen.jsx
@@ -14,6 +14,7 @@ import FoodCard from "../components/FoodCard";
 
 export default function DashboardScreen() {
   const [recipes, setRecipes] = useState(null);
+  const [simpleRecipes, setSimpleRecipes] = useState(null);
   const [currentPage, setCurrentPage] = useState("dashboard");
   const [selectedElement, setSelectedElement] = useState(null);
 
@@ -50,7 +51,29 @@ export default function DashboardScreen() {
     };
 
     fetchData();
+
+    const fetchSimpleData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.spoonacular.com/recipes/complexSearch?query=easy&number=5&apiKey=affec1406a384e37982659a430a102d9"
+        );
+
+        if (!response.ok) {
+          console.error("HTTP error", response.status);
+          return;
+        }
+
+        const data = await response.json();
+        setSimpleRecipes(data.results);
+      } catch (error) {
+        console.error("Fetch error", error);
+      }
+    };
+
+    fetchSimpleData();
+
   }, []);
+
 
   if (!fontsLoaded) {
     return <AppLoadingScreen />;
@@ -64,20 +87,37 @@ export default function DashboardScreen() {
         <View style={styles.contentMargin}>
           <Text style={styles.title}>COOKKING</Text>
           <SearchBar placeholder="Buscar una receta" />
-          <Text style={styles.title}>Aleatorias</Text>
-          <ScrollView horizontal>
-            {recipes &&
-              recipes.map((element) => (
-                <FoodCard
-                  key={element.id}
-                  title={element.title}
-                  image={element.image}
-                  onPress={() => {
-                    // Aqui va el elemento
-                    goToSummary(element);
-                  }}
-                />
-              ))}
+          <ScrollView>
+            <Text style={styles.title}>Aleatorias</Text>
+            <ScrollView horizontal style={styles.scrollView}>
+              {recipes &&
+                recipes.map((element) => (
+                  <FoodCard
+                    key={element.id}
+                    title={element.title}
+                    image={element.image}
+                    onPress={() => {
+                      // Aqui va el elemento
+                      goToSummary(element);
+                    }}
+                  />
+                ))}
+            </ScrollView>
+            <Text style={styles.title}>Simples</Text>
+            <ScrollView horizontal style={styles.scrollView}>
+              {simpleRecipes &&
+                simpleRecipes.map((element) => (
+                  <FoodCard
+                    key={element.id}
+                    title={element.title}
+                    image={element.image}
+                    onPress={() => {
+                      // Aqui va el elemento
+                      goToSummary(element);
+                    }}
+                  />
+                ))}
+            </ScrollView>
           </ScrollView>
         </View>
       </View>
@@ -93,12 +133,15 @@ const styles = StyleSheet.create({
     justifyContent: "start",
     marginTop: 25,
   },
+  scrollView: {
+    height: 'auto'
+  },
   title: {
     fontFamily: "MoreSugar",
     fontSize: 28,
     marginVertical: 20,
   },
   contentMargin: {
-    marginHorizontal: 50,
+    marginHorizontal: 40,
   },
 });
